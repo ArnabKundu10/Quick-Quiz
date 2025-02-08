453afc4837db488da2e0d2aec2c125eb0de015b2
Arnab <arnabk2001@gmail.com>
2025-02-08 20:07:14 +0530

    creation of ques.json file to store quiz data

--- backend/index.js
+++ backend/index.js
@@ -16,16 +16,17 @@ app.use(
 app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-console.log(__dirname);
+// console.log(__dirname);
+    const filepath=path.join(__dirname,"ques.json");
+    console.log(filepath);
+    const response=fs.readFileSync("ques.json");
+        const data = JSON.parse(response);
 app.get("/auth", async (req, res) => {
   try {
-    const response = await axios.get("https://api.jsonserve.com/Uw5CrX");
-    const data = response.data;
-    // const filepath=path.join(__dirname,"ques.json");
-    // console.log(filepath);
-    // const response=fs.readFileSync("ques.json");
-    // const data =await JSON.parse(response);
-    fs.writeFileSync("ques.js", JSON.stringify(data, null, 2));
+    // const response = await axios.get("https://api.jsonserve.com/Uw5CrX");
+    // const data = response.data;
+
+    // fs.writeFileSync("ques.js", JSON.stringify(data, null, 2));
     res.status(201).json(data);
   } catch (error) {
     console.log(error);
